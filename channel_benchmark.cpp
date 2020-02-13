f93d197351e0b89ce77c5fb9ef775305d9651808
Andrei Avram <avramandrei@ymail.com>
2020-02-13 09:23:39 +0200

    Add benchmark.

---
+++ channel_benchmark.cpp
@@ -0,0 +1,56 @@
+#include "channel.h"
+
+#include <benchmark/benchmark.h>
+
+/*
+ ---------------------------------------------------------------
+ Benchmark                     Time             CPU   Iterations
+ ---------------------------------------------------------------
+ BM_ChannelWithInt           121 ns          121 ns      5628541
+ BM_ChannelWithString        235 ns          235 ns      2962239
+ BM_ChannelWithStruct        246 ns          246 ns      2864420
+ */
+
+struct Entry {
+    int id{};
+    std::string label = "label";
+};
+
+static void BM_ChannelWithInt(benchmark::State &state) {
+    Channel<int> channel{1};
+    int in = 1;
+    int out = 0;
+    for (auto _ : state) {
+        in >> channel;
+        out << channel;
+    }
+}
+
+BENCHMARK(BM_ChannelWithInt);
+
+static void BM_ChannelWithString(benchmark::State &state) {
+    Channel<std::string> channel{1};
+    std::string in = "input";
+    std::string out;
+    for (auto _ : state) {
+        in >> channel;
+        out << channel;
+    }
+}
+
+BENCHMARK(BM_ChannelWithString);
+
+
+static void BM_ChannelWithStruct(benchmark::State &state) {
+    Channel<Entry> channel{1};
+    Entry in{};
+    Entry out{};
+    for (auto _ : state) {
+        in >> channel;
+        out << channel;
+    }
+}
+
+BENCHMARK(BM_ChannelWithStruct);
+
+BENCHMARK_MAIN();
