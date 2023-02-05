ad6f54abf682cee635470342e93ec09cea2c39dc
Andrei Avram <avramandrei@ymail.com>
2023-02-05 21:55:21 +0200

    Run benchmark.

--- tests/channel_benchmark.cpp
+++ tests/channel_benchmark.cpp
@@ -5,7 +5,7 @@
 /**
     Results on release build (GCC 10) with CPU scaling disabled
 
-    2022-03-20T23:00:06+02:00
+    2023-02-05T20:35:06+02:00
     Running channel_benchmark
     Run on (8 X 3595.91 MHz CPU s)
     CPU Caches:
@@ -13,13 +13,13 @@
       L1 Instruction 32 KiB (x4)
       L2 Unified 256 KiB (x4)
       L3 Unified 8192 KiB (x1)
-    Load Average: 3.29, 2.79, 2.33
+    Load Average: 0.78, 0.82, 0.84
     ---------------------------------------------------------------
     Benchmark                     Time             CPU   Iterations
     ---------------------------------------------------------------
-    BM_ChannelWithInt          42.0 ns         42.0 ns     15997933
-    BM_ChannelWithString       46.3 ns         46.3 ns     15363996
-    BM_ChannelWithStruct       47.6 ns         47.6 ns     14817823
+    BM_ChannelWithInt          31.3 ns         31.3 ns     21684083
+    BM_ChannelWithString       37.1 ns         37.1 ns     18832698
+    BM_ChannelWithStruct       37.6 ns         37.6 ns     18625765
  */
 
 struct Entry {
