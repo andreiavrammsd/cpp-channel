ef527dc57f8b370eec65ed2626acbc3631e37672
Andrei Avram <6795248+andreiavrammsd@users.noreply.github.com>
2025-06-14 15:53:44 +0000

    Streaming on main thread

--- examples/streaming.cpp
+++ examples/streaming.cpp
@@ -18,13 +18,9 @@ int main()
     const auto input = [](messages& chan, std::size_t thread, std::chrono::milliseconds pause) {
         thread_local static std::size_t inc = 0U;
 
-        while (true) {
-            if (chan.closed()) {
-                return;
-            }
-
+        while (!chan.closed()) {
             ++inc;
-            chan << std::string{std::to_string(inc) + " from: " + std::to_string(thread)};
+            chan << std::string{"Streaming " + std::to_string(inc) + " from thread " + std::to_string(thread)};
 
             std::this_thread::sleep_for(pause);
         }
@@ -35,12 +31,6 @@ int main()
         in_futures.push_back(std::async(input, std::ref(channel), i, std::chrono::milliseconds{500}));
     }
 
-    // Stream incoming data to a destination
-    const auto out = [](messages& chan, std::ostream& stream, const std::string& separator) {
-        std::move(chan.begin(), chan.end(), std::ostream_iterator<std::string>(stream, separator.c_str()));
-    };
-    const auto out_future = std::async(out, std::ref(channel), std::ref(std::cout), "\n");
-
     // Close the channel after some time
     const auto timeout = [](messages& chan, std::chrono::milliseconds after) {
         std::this_thread::sleep_for(after);
@@ -48,7 +38,10 @@ int main()
     };
     const auto timeout_future = std::async(timeout, std::ref(channel), std::chrono::milliseconds{3000U});
 
-    out_future.wait();
+    // Stream incoming data to a destination
+    std::move(channel.begin(), channel.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+
+    // Wait for other threads
     for (auto& future : in_futures) {
         future.wait();
     }
